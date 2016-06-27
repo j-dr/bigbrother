33994688558c782d9944eaf3ec6b597bd2d56b1f
Joe <joe.derose13@gmail.com>
2016-06-27 14:03:28 -0700

    Fixed return statement

--- bigbrother/basecatalog.py
+++ bigbrother/basecatalog.py
@@ -160,6 +160,8 @@ class BaseCatalog:
                 nf = len(fieldmap[ft][mapkey])
                 mapunit[mapkey] = mapunit[mapkey].view(dt).reshape((ne,nf))
 
+        return mapunit
+
     def readMappable(self, mappable, fieldmap):
         """
         Default reader is FITS reader
