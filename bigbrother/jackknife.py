d2affe11fd104806be9f0895b95d1975b14f06c5
j-dr <joe.derose13@berkeley.edu>
2016-07-18 12:01:46 -0700

    Started refactoring jackknife

---
+++ bigbrother/jackknife.py
@@ -0,0 +1,27 @@
+from __future__ import print_function, division
+import numpy as np
+
+
+class Jackknife:
+
+    def __init__(self, jtype='healpix', nside=8, nneighbors=1, radius=None,
+                nregions=None):
+
+        self.jtype      = jtype
+        self.nside      = nside
+        self.nneighbors = nneighbors
+        self.radius     = radius
+        self.nregions   = nregions
+
+
+    def healpixJackknifeRegions(self):
+
+        pass
+
+    def subboxJackknifeRegions(self):
+
+        pass
+
+    def combineJackknife(self):
+
+        pass
