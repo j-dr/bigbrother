1c2030a6b90d4585b2d3cedd29e5a108ac137a3f
Amara <amccune2@stanford.edu>
2016-08-14 00:13:53 -0700

    Adding new code

--- bigbrother/selection.py
+++ bigbrother/selection.py
@@ -84,7 +84,7 @@ class Selector:
         seletion -- dict
         A dictionary specifying the selection information. Needs
         to have keys: bins, mapkeys, lower, selection_ind.
-        
+
         returns
         --------
         A list of functions of length given by the number of
@@ -92,10 +92,10 @@ class Selector:
         - Take in an array of data as their only argument
         - Return an array of length of the input data, containing
           True or False in each element depending on whether or not
-          the corresponding element of the input array satisfied 
+          the corresponding element of the input array satisfied
           the selection criterea.
-          
-        
+
+
         """
         sfunctions = []
 
@@ -120,6 +120,64 @@ class Selector:
             sfunctions.append(sf)
 
         return sfunctions
+
+    def cut2DSelection(self, selection):
+        """
+        Create a list of functions which create boolean indices
+        for a 2-dimensional cut for various types of galaxy samples.
+
+        inputs
+        ------
+        selection - dictionary specifying selection data.
+        Needs the keys: mapkeys, int1, slope1, int2, slope2, lower1,
+        lower2, bins1, bins2.
+
+        returns
+        -------
+        Two arrays of functions, with size given by the size of the
+        input data (colorx or colory). Each element contains True or False
+        depending on whether or not the corresponding input element fulfills
+        the selection criteria.
+        """
+        #Create the sfunctions to contain the index-generating functions.
+        sfunctions1 = []
+        sfunctions2 = []
+
+        #Calculate the color arrays from mapkeys (three columns corresponding to magnitude data).
+        #Two mapkeys for a 2D selection.
+        mk1 = selection['mapkeys'][0] - selection['mapkeys'][1]
+        mk2 = selection['mapkeys'][0] - selection['mapkeys'][2]
+
+        #Iterate over the bins and make an array of functions that create indices.
+        #Returns two arrays, one for each axis.
+        for i in range (len(selection['bins'])):
+            if selection['lower1']:
+                if selection['lower2']:
+                    if selection['int1'] is None or selection['slope1'] is None:
+                        if selection ['int2'] is None or selection ['slope2'] is None: sf1 = lambda data : selection['bins1'][i] <= data[mk1], sf2 = lambda data : selection['bins2'][i] <= data[mk2]
+                        else: si = int1 + (slope1*i), sf1 = lambda data : selection['bins1'][i] <= data[mk1][:,si], sf2 = lambda data : selection['bins2'][i] <= data[mk2]
+                    else: si1 = int1 + (slope1*i), si2 = int2 + (slope2*i), sf1 = lambda data : selection['bins1'][i] <= data[mk1][:,si1], sf2 = lambda data : selection['bins2'][i] <= data[mk2][:,si2]
+                else:
+                    if selection['int1'] is None or selection['slope1'] is None:
+                        if selection ['int2'] is None or selection ['slope2'] is None: sf1 = lambda data : selection['bins1'][i] <= data[mk1], sf2 = lambda data : selection['bins2'][i] >= data[mk2]
+                        else: si = int1 + (slope1*i), sf1 = lambda data : selection['bins1'][i] <= data[mk1][:,si], sf2 = lambda data : selection['bins2'][i] >= data[mk2]
+                    else: si1 = int1 + (slope1*i), si2 = int2 + (slope2*i), sf1 = lambda data : selection['bins1'][i] <= data[mk1][:,si1], sf2 = lambda data : selection['bins2'][i] >= data[mk2][:,si2]
+            else:
+                if selection['lower2']:
+                    if selection['int1'] is None or selection['slope1'] is None:
+                        if selection ['int2'] is None or selection ['slope2'] is None: sf1 = lambda data : selection['bins1'][i] >= data[mk1], sf2 = lambda data : selection['bins2'][i] <= data[mk2]
+                        else: si = int1 + (slope1*i), sf1 = lambda data : selection['bins1'][i] >= data[mk1][:,si], sf2 = lambda data : selection['bins2'][i] <= data[mk2]
+                    else: si1 = int1 + (slope1*i), si2 = int2 + (slope2*i), sf1 = lambda data : selection['bins1'][i] >= data[mk1][:,si1], sf2 = lambda data : selection['bins2'][i] <= data[mk2][:,si2]
+                else:
+                    if selection['int1'] is None or selection['slope1'] is None:
+                        if selection ['int2'] is None or selection ['slope2'] is None: sf1 = lambda data : selection['bins1'][i] >= data[mk1], sf2 = lambda data : selection['bins2'][i] >= data[mk2]
+                        else: si = int1 + (slope1*i), sf1 = lambda data : selection['bins1'][i] >= data[mk1][:,si], sf2 = lambda data : selection['bins2'][i] >= data[mk2]
+                    else: si1 = int1 + (slope1*i), si2 = int2 + (slope2*i), sf1 = lambda data : selection['bins1'][i] >= data[mk1][:,si1], sf2 = lambda data : selection['bins2'][i] >= data[mk2][:,si2]
+
+            sfunctions1.append(sf1)
+            sfunction2.append(sf2)
+
+        return sfunctions1, sfunctions2
 
     def mapArray(self):
         """
