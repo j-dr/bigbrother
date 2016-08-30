Wrote color_dist metric, fixed calculation of error on fractional differences in gmetric

bigbrother/magnitudemetric.py
@@ -484,12 +484,105 @@ class ColorDist(GMetric):
     def __init__(self, ministry, zbins=None, cbins=None,
                     catalog_type=['galaxycatalog'],
                     usebands=None, appmag=False, amagcut=None,
-                    **kwargs):
+                    pdf=False, **kwargs):
 
         GMetric.__init__(self, ministry,
                             catalog_type=catalog_type,
-        )
+                            **kwargs)
 
+        if zbins is None:
+            self.zbins = np.linspace(self.ministry.minz,
+                                      self.ministry.maxz,
+                                      10)
+        else:
+            self.zbins = zbins
+
+        self.nzbins = len(self.zbins)
+
+        self.pdf = pdf
+
+        if cbins is None:
+            self.cbins = np.linspace(-2, 2, 61)
+        else:
+            self.cbins = cbins
+        self.ncbins = len(self.cbins) - 1
+
+        if appmag:
+            self.mkey = 'appmag'
+        else:
+            self.mkey = 'luminosity'
+            if amagcut is None:
+                self.amagcut = -19
+            else:
+                self.amagcut = amagcut
+
+        if usebands is None:
+            self.usebands = [[0, 1]]
+        else:
+            self.usebands = usebands
+
+        self.ncolors = len(self.usebands)
+
+        self.cd = None
+
+    @jackknifeMap
+    def map(self, mapkey):
+
+        if self.cd is None:
+            self.cd = np.zeros(self.njack, self.ncbins,
+                                self.ncolors, self.nzbins)
+
+        clr = np.zeros((len(mapunit[self.mkey]),
+                        self.ncolors))
+        for c in range(self.ncolors):
+            clr[:,c] = mapunit[self.mkey][:,self.usebands[c][0]] - mapunit[self.mkey][:,self.usebands[c][1]]
+
+        for i, z in enumerate(self.zbins):
+            zlidx = mapunit['redshift'].searchsorted(self.zbins[i])
+            zhidx = mapunit['redshift'].searchsorted(self.zbins[i+1])
+            if self.mkey == 'luminosity':
+                lidx = mapunit[self.mkey][zlidx:zhidx] < self.amagcut
+            else:
+                lidx = slice(0,zhidx-zlidx)
+
+                for ci in range(self.ncolors):
+                    c, e = np.histogram(clr[zlidx:zhidx,ci][lidx], bins=self.cbins)
+
+                    self.cd[self.jcount,:,ci,i] += c
+
+    def reduce(self, rank=None, comm=None):
+
+        if rank is not None:
+            gcd = comm.gather(self.cd, root=0)
+
+            gshape = [self.cd.shape[i] for i in range(len(self.cd.shape))]
+            gshape[0] = self.njacktot
+
+            if rank==0:
+                self.cd = np.zeros(gshape)
+                jc = 0
+                for i, g in enumerate(gcd):
+                    nj = g.shape[0]
+                    self.cd[jc:jc+nj,:,:,:] = g
+
+                    jc += nj
+                if self.pdf:
+                    self.tc = np.sum(self.cd, axis=1)
+                    self.jcolor_dist = self.cd / self.tc
+                else:
+                    area = self.getArea()
+                    self.jcolor_dist = self.cd / area
+
+                self.jcolor_dist, self.color_dist, self.varcolor_dist = self.jackknife(self.jcolor_dist)
+        else:
+            if self.pdf:
+                self.tc = np.sum(self.cd, axis=1)
+                self.jcolor_dist = self.cd / self.tc
+            else:
+                area = self.getArea()
+                self.jcolor_dist = self.cd / area
+
+            self.jcolor_dist, self.color_dist, self.varcolor_dist = self.jackknife(self.jcolor_dist)
 
 class ColorColor(Metric):
     """

bigbrother/metric.py
@@ -150,11 +150,14 @@ class GMetric(Metric):
         pass
 
     def visualize(self, plotname=None, usecols=None,
-                    usez=None,fracdev=False, sharex=True, sharey=True,
-                    ref_y=None, ref_x=[None], xlim=None, ylim=None,
+                    usez=None,fracdev=False, sharex=True,
+                    sharey=True,
+                    ref_y=None, ref_x=[None], ref_ye=None,
+                    xlim=None, ylim=None,
                     fylim=None, f=None, ax=None, label=None,
                     xlabel=None, ylabel=None,compare=False,
-                    logx=False, logy=True, rusecols=None, **kwargs):
+                    logx=False, logy=True, rusecols=None,
+                    **kwargs):
         """
         Plot the calculated metric.
 
@@ -208,7 +211,6 @@ class GMetric(Metric):
 
         if usez is None:
             usez = range(self.nzbins)
-        print(usez)
         nzbins = len(usez)
 
         #If want to plot fractional deviations, and ref_y
@@ -278,12 +280,21 @@ class GMetric(Metric):
                             ax[i][j].set_yscale('log')
                     else:
                         rb = rusecols[i]
+                        #calculate error on fractional
+                        #difference
+                        if (ref_ye is not None) & (self.ye is not None):
+                            vye = self.ye**2
+                            vrye = ref_ye**2
+                            dye = np.sqrt(((1 - self.y[li:hi,b,j]) / ref_y[:,rb,j] - (ref_y[:,rb,j] - self.y[li:hi,b,j] / ref_y[:,rb,j] ** 2)) * ref_ye[:,rb,j] + (ref_y[:,rb,j] - 1) / ref_y[:,rb,j] * self.ye[li:hi,b,j])
+                        else self.ye is not None:
+                            dye = None
+
                         l1 = ax[2*i][j].errorbar(mxs, self.y[:,b,j],
                                           self.ye[:,b,j], barsabove=True, **kwargs)
                         ax[2*i+1][j].errorbar(mxs[li:hi],
                                               (self.y[li:hi,b,j]-ref_y[:,rb,j])\
                                               /ref_y[:,rb,j],
-                                              yerr=self.ye[li:hi,b,j],
+                                              yerr=dye,
                                               barsabove=True,
                                               **kwargs)
                         if logx:
@@ -326,6 +337,15 @@ class GMetric(Metric):
 
                 else:
                     rb = rusecols[i]
+                    #calculate error on fractional
+                    #difference
+                    if (ref_ye is not None) & (self.ye is not None):
+                        vye = self.ye**2
+                        vrye = ref_ye**2
+                        dye = np.sqrt(((1 - self.y[li:hi,b,0]) / ref_y[:,rb,0] - (ref_y[:,rb,0] - self.y[li:hi,b,0] / ref_y[:,rb,0] ** 2)) * ref_ye[:,rb,0] + (ref_y[:,rb,0] - 1) / ref_y[:,rb,0] * self.ye[li:hi,b,0])
+                    else self.ye is not None:
+                        dye = None
+
                     l1 = ax[2*i][0].errorbar(mxs, self.y[:,b,0],
                                               yerr=self.ye[:,b,0],
                                               barsabove=True,